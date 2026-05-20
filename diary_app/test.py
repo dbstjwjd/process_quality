0530347c12239a0b6b24948d79d39ad5c82695cd
dbstjwjd <wjddbstj1@naver.com>
2026-05-20 12:38:21 +0900

    불필요한 폴더 제거

--- diary_app/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtGui import QFontDatabase
-
-app = QApplication(sys.argv)
-
-id = QFontDatabase.addApplicationFont("assets/fonts/Pretendard-Regular.otf")
-print("폰트 ID:", id)
-print("패밀리명:", QFontDatabase.applicationFontFamilies(id))
